6fd93f99ac91f27b26e97694fb296487a2b87996
Jagruti Patel <jagrutipatel200218@gmail.com>
2024-11-01 23:43:38 +0530

    inition commit

---
+++ src/content_option.js
@@ -0,0 +1,124 @@
+
+const logotext = "Jagruti Patel";
+
+const meta = {
+    title: "Jagruti Patel",
+    description: "I'm Jagruti Patel, a Web Developer.",
+};
+
+const introdata = {
+    title: "Hi, I'm Jagruti Patel",
+    animated: {
+        first: "Front-End Developer",
+        second: "Back-End Developer",
+        third: "Full-Stack Developer",
+        fourth: "Web Programmer",
+        fifth: "Web Designer",
+        sixth: "Web Application Developer",
+        
+    },
+    description: "Passionate web developer with a strong foundation in front-end and back-end technologies. Dedicated to creating user-friendly and visually appealing web applications that enhance the digital experience.",
+    your_img_url: "https://avatars.githubusercontent.com/u/90373779?s=400&u=61edfe9bfe042809e3d3d1047fb2966c6877ff8c&v=4",
+};
+
+const dataabout = {
+    title: "Who am I?",
+    aboutme: "I'm a passionate Web Developer with a B.Sc. in Information Technology. I enjoy creating clean, responsive, and user-friendly interfaces that make a positive impact. With hands-on experience from internships and a strong foundation in HTML, CSS, JavaScript, and MySQL, I’m always looking for creative ways to bring ideas to life through code.I'm committed to continuous learning and improving my skills to keep up with the latest trends and technologies. My goal is to build digital experiences that are both functional and aesthetically pleasing, making the web more accessible and engaging for all.",
+};
+const worktimeline = [{
+        jobtitle: "Web Development Intern",
+        where: "Fi Havock Digital P. Ltd.",
+        date: "29 July 2024 - Present",
+    },
+    {
+        jobtitle: "Web Development Intern",
+        where: "TechnoHacks EduTech ",
+        date: "15 Feb 2024 - 14 Mar 2024",
+    },
+    
+];
+
+const skills = [{
+        name: "HTML",
+    },
+    
+    {
+        name: "CSS",
+    },
+    
+    {
+        name: "JavaScript",
+    },
+    
+    {
+        name: "React",
+    },
+
+    {
+        name: "Java",
+    },
+
+    {
+        name: "PHP",
+    },
+
+    {
+        name: "C++",
+    },
+
+    {
+        name: "MySQL",
+    },
+];
+
+const dataportfolio = [{
+    title: "Make a Balloon And Bust On Tap",
+    description: "Created an interactive balloon-popping game using the Phaser 3 framework. Players inflate balloons with a virtual pump button until they reach a set size and float upwards. Players can tap on the balloons to make them burst, removing the balloon, thread, and letter from the screen.",
+    technologies: "Phaser 3, JavaScript",
+    url: "https://jagrutipatel821.github.io/Make_a_Balloon_And_burst_On_Tap/"
+  },
+  {
+    title: "Fi Havock Digital Website Redesign",
+    description: "Contributed to the redesign of Fi Havock Digital’s main website, enhancing loading speeds, user experience, and SEO performance. Focused on creating a smoother user experience while ensuring mobile responsiveness.",
+    technologies: "HTML, CSS, JavaScript, SEO Optimization",
+    url: ""
+  },
+  {
+    title: "Calculator App",
+    description: "Developed a simple calculator application for basic operations. This project showcases my skills in creating functional user interfaces and implementing JavaScript for real-time calculations.",
+    technologies: "HTML, CSS, JavaScript",
+    url: ""
+  },
+  {
+    title: "Landing Page for Techno Hacks EduTech",
+    description: "Built a clone landing page for Techno Hacks EduTech using HTML and CSS. This project demonstrates my ability to replicate designs and create responsive layouts.",
+    technologies: "HTML, CSS",
+    url: ""
+  },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "jagrutipatel2005103@gmail.com",
+    description: "Looking forward to hearing from you!",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_l0c6jzy",
+    YOUR_TEMPLATE_ID: "template_50mt2re",
+    YOUR_USER_ID: "TByS9kNuZqxkxBNs7",
+};
+
+const socialprofils = {
+    github: "https://github.com/jagrutipatel821",
+    linkedin: "https://www.linkedin.com/authwall?trk=bf&trkInfo=AQF_SnKIuhZgtgAAAZLoDXjQsKiHXsoLQvCwqsJ0hF7xDn0D49cczxKWQ_gR9bKv32-jk7StlsnOpFfDUr-alRujoHgZKoCYsK2M-6KeaxLc1ooT0iEC3hFKB6giZemYljLArDc=&original_referer=&sessionRedirect=https%3A%2F%2Fwww.linkedin.com%2Fin%2Fpateljagruti",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
